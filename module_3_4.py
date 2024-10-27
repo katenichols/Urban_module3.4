cf91ca1ed953d3e70b761796d22297433637daa2
Ekaterina Suslova <ekaterina.suslova.job@gmail.com>
2024-10-27 18:49:28 +0500

    ДЗ по модулю 3.4

---
+++ module_3_4.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word, *other_words):
+  same_words=[]
+  for i in other_words:
+    if i.lower().find(root_word.lower()) >= 0 or root_word.lower().find(i.lower()) >=0:
+      same_words.append(i)
+  
+  return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+
+print(result2)
